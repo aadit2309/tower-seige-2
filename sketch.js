adb38bd0428ed8da3dec6c7f9906ccf8242edd78
aadit2309 <68148484+aadit2309@users.noreply.github.com>
2020-11-02 17:05:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var ground1;
-
+var score = 0;
 
 function setup() {
   createCanvas(800,400);
@@ -13,35 +13,52 @@ function setup() {
 
 
   ground1 = new Ground (400,300,300,10);
-  box8 = new box (330,235,30,40);
-  box9 = new box (360,235,30,40);
-  box10 = new box (390,235,30,40);
-  box11= new box (420,235,30,40);
-  box12 = new box (450,235,30,40);
-  box13 = new box (360,195,30,40);
-  box14 = new box (390,195,30,40);
-  box15 = new box (420,195,30,40);
-  box16 = new box (390,155,30,40);
+  box1 = new box (330,235,30,40);
+  box2 = new box (360,235,30,40);
+  box3 = new box (390,235,30,40);
+  box4 = new box (420,235,30,40);
+  box5 = new box (450,235,30,40);
+  box6 = new box (360,195,30,40);
+  box7 = new box (390,195,30,40);
+  box8 = new box (420,195,30,40);
+  box9 = new box (390,155,30,40);
 
   polygon = Bodies.circle(50,200,20);
   World.add(world,polygon);
 
   slingShot = new SlingShot (polygon,{x:100,y:200});
+
+  
   
 }
 
 function draw() {
-  background(255,255,255); 
+ 
   Engine.update(engine); 
+  text("Score  " + score, width-300, 50);
+ 
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box5.display();
+  box6.display();
+  box7.display();
   box8.display();
   box9.display();
-  box10.display();
-  box11.display();
-  box12.display();
-  box13.display();
-  box14.display();
-  box15.display();
-  box16.display();
+  
+  
+  box1.score();
+  box2.score();
+  box3.score();
+  box4.score();
+  box5.score();
+  box6.score();
+  box7.score();
+  box8.score();
+  box9.score();
 
   ellipseMode(RADIUS);
   ellipse(polygon.position.x,polygon.position.y,20,20)
@@ -67,8 +84,17 @@ function keyPressed(){
   }
 }
 
-function remove(){
-  if(box.velocity>3){
-    box.destroy;
+async function getBackgroundImg(){
+  var response = await fetch("http://worldclockapi.com/api/json/est/now");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=0600 && hour<=1900){
+      bgcolour = "black";
+  }
+  else{
+      bgcolour = "yellow";
   }
 }
